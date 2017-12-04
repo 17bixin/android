893010ed2e51e0cb1524284207586d700c702f8d
guojia <quchunlin@newborntown.com>
2017-12-04 13:12:54 +0800

    闪光灯功能

--- app/src/main/java/com/gestures/heart/record/ShortVideoRecordActivity.java
+++ app/src/main/java/com/gestures/heart/record/ShortVideoRecordActivity.java
@@ -1,11 +1,15 @@
 package com.gestures.heart.record;
 
 import android.content.Context;
+import android.content.pm.FeatureInfo;
+import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraManager;
 import android.opengl.EGL14;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -144,6 +148,8 @@ public class ShortVideoRecordActivity extends BaseActivity implements Camera.Pre
     private RecyclerView mEffectRecyclerView;
     private EffectAndFilterSelectAdapter mEffectRecyclerAdapter;
     private BottomSheetBehavior<View> behavior;
+    private CameraManager mCameraManager;
+    private boolean mLightStatus = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -256,9 +262,14 @@ public class ShortVideoRecordActivity extends BaseActivity implements Camera.Pre
             @Override
             protected void initEvent() {
                 iv_flash_btn.setOnClickListener(new View.OnClickListener() {
+
+
+
                     @Override
                     public void onClick(View v) {
                         // 闪光灯开关
+                        lightSwitch(mLightStatus);
+                        mLightStatus = !mLightStatus;
                     }
                 });
                 iv_delay_btn.setOnClickListener(new View.OnClickListener() {
@@ -1055,6 +1066,56 @@ public class ShortVideoRecordActivity extends BaseActivity implements Camera.Pre
         mCreateItemThread = null;
         mCreateItemHandler = null;
     }
+
+    /**
+     * 手电筒控制方法
+     *
+     * @param lightStatus
+     * @return
+     */
+    private void lightSwitch(final boolean lightStatus) {
+        if(mCameraManager == null){
+            mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        }
+        if (lightStatus) { // 关闭手电筒
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                try {
+                    mCameraManager.setTorchMode("0", false);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+        } else { // 打开手电筒
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                try {
+                    mCameraManager.setTorchMode("0", true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                final PackageManager pm = getPackageManager();
+                final FeatureInfo[] features = pm.getSystemAvailableFeatures();
+                for (final FeatureInfo f : features) {
+                    if (PackageManager.FEATURE_CAMERA_FLASH.equals(f.name)) { // 判断设备是否支持闪光灯
+                        if (null == mCamera) {
+                            mCamera = Camera.open();
+                        }
+                        final Camera.Parameters parameters = mCamera.getParameters();
+                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        mCamera.setParameters(parameters);
+                        mCamera.startPreview();
+                    }
+                }
+            }
+        }
+    }
+
 
 
 }
